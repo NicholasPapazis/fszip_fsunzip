81245083b61fa29bd18327ed58c68fd89cd3a9a7
Nicholas Papazis <nicholasepapazis@gmail.com>
2025-03-16 18:41:15 -0400

    clean up code

--- fsunzip.c
+++ fsunzip.c
@@ -10,36 +10,43 @@ int main(int argc, char** argv) {
         return 1; //exit with status code 1
     }
 
+    //loop through all files specifed on command line
     for (int i = 1; i < argc; i++) {
-        FILE* inputFile = fopen(argv[i], "rb");
-        if (inputFile == NULL) {
-            perror("could not open file");
-            return 1;
+
+        FILE* fp = fopen(argv[i], "rb"); //open zip file
+
+        //if fp is NULL, then there was an issue opening the file
+        if (fp == NULL) {
+            exit(1); //exit with status code 1
         }
 
-        int32_t rLen;
-        char curr;
 
-        while (fread(&rLen, sizeof(int32_t), 1, inputFile) == 1) {
-            if (fread(&curr, sizeof(char), 1, inputFile) != 1) {
+        int32_t rLen; //holds 4-byte run length of character
+        char curr; //holds character that will be decompressed
+
+        //loop through compressed zip file entries
+        while (fread(&rLen, sizeof(int32_t), 1, fp) == 1) {
+            //checks if reading character failed
+            if (fread(&curr, sizeof(char), 1, fp) != 1) {
                 fprintf(stderr, "could not read character\n");
-                fclose(inputFile);
+                fclose(fp);
                 return 1;
             }
 
+            //loop the run length
             for (int i = 0; i < rLen; i++) {
-                printf("%c", curr); //print to stdout
+                printf("%c", curr); //print current character to stdout
             }
 
-            //check for new line character
-            if (fread(&curr, sizeof(char), 1, inputFile) == 1 && curr == '\n') {
-                printf("%c", curr); //print new line
+            //if we read character successfully, and it is a new line character
+            if (fread(&curr, sizeof(char), 1, fp) == 1 && curr == '\n') {
+                printf("%c", curr); //print the new line character to stdout
             } else {
-                ungetc(curr, inputFile);
+                ungetc(curr, fp); //pushes c back to stream, if next char is not a new line character
             }
         }
 
-        fclose(inputFile);
+        fclose(fp); //close file
 
     }
     return 0;
--- fszip.c
+++ fszip.c
@@ -7,46 +7,59 @@
 
 int main(int argc, char** argv) {
 
+    //if no files are specified, the program will exit with return code 1
     if (1 == argc) {
         printf("fszip: file1 [file2 ...]\n");
         return 1;
     }
 
+    //loop through all files specifed on command line
     for (int i = 1; i < argc; i++) {
         char line[MYLIMIT];
         FILE* fp = fopen(argv[i], "r");
+        //check if file opened successfully
         if (fp == NULL) {
             perror("Error opening file");
         }
 
+        //iterate line by through input file and compress the content.
         while (fgets(line, MYLIMIT, fp) != NULL) {
-            int rLen = 1;
-            char curr = line[0];
-            char prev = '\0';
-            int len = strlen(line);
+            int rLen = 1; //holds the length of each character
+            char curr = line[0]; //holds the character while we are counting its run length
+            char prev = '\0'; //holds previous value
+            int len = strlen(line); //length of line we are currently on
 
+            //iterates through each character in the current line
             for (int j = 1; j < len; j++) {
-                prev = curr;
-                curr = line[j];
+                prev = curr; //update previous to hold the current character
+                curr = line[j]; //set curr to the next character in the line
 
+                //checks if we have consecutive equal characters
                 if (curr == prev) {
-                    rLen++;
+                    rLen++; //if we do have consecutive equal characters, then increment run length
                 } else {
-                    fwrite(&rLen, sizeof(int32_t), 1, stdout);
-                    fwrite(&prev, sizeof(char), 1, stdout);
-                    rLen = 1;
+                    //if we do not have consecutive equal characters
+                    fwrite(&rLen, sizeof(int32_t), 1, stdout); //write out a 4-byte integer (the run length) in binary format
+                    fwrite(&prev, sizeof(char), 1, stdout); //the single character in ASCII
+                    rLen = 1; //reset run length
                 }
             }
+
+            //make sure the line is not empty
             if(len > 0){
-                fwrite(&rLen, sizeof(int32_t), 1, stdout);
-                fwrite(&curr, sizeof(char), 1, stdout);
+                //write the last run of characters on a line
+                fwrite(&rLen, sizeof(int32_t), 1, stdout); //write out a 4-byte integer (the run length) in binary format
+                fwrite(&curr, sizeof(char), 1, stdout); //the single character in ASCII
 
             }
 
-        }
-        fwrite("\n", sizeof(char), 1, stdout);
-        fclose(fp);
-    }
+        }//end while
+
+        fwrite("\n", sizeof(char), 1, stdout);  //write newline character at end of each line
+        fclose(fp); //close file
+
+    }//end for
 
     return 0;
+
 }
